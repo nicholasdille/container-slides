d807d00a769cc6b47aca8291d1ef0b30c9c3419e
Nicholas Dille <nicholas.dille@mailbox.org>
2020-01-22 22:50:48 +0100

    Fixed demos

--- 070_testing/goss/prep.sh
+++ 070_testing/goss/prep.sh
@@ -3,4 +3,5 @@
 curl -sLf https://api.github.com/repos/aelsabbahy/goss/releases/latest | jq --raw-output '.assets[] | select(.name | contains("-linux-amd64")) | .browser_download_url' | xargs curl -sLfo /usr/local/bin/goss
 chmod +x /usr/local/bin/goss
 
-curl -sLf https://api.github.com/repos/aelsabbahy/goss/releases/latest | jq --raw-output '.assets[] | select(.name == "dgoss") | .browser_download_url' | xargs curl -sLfO
+curl -sLf https://api.github.com/repos/aelsabbahy/goss/releases/latest | jq --raw-output '.assets[] | select(.name == "dgoss") | .browser_download_url' | xargs curl -sLfo /usr/local/bin/dgoss
+chmod +x /usr/local/bin/dgoss
