b0317bcc5b61c2b890a972560ef0bac4667ea19d
Nicholas Dille <nicholas.dille@mailbox.org>
2020-01-22 11:09:47 +0100

    Improved restart ability for demo

--- 020_advanced/140_multi_arch/clean.sh
+++ 020_advanced/140_multi_arch/clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
-docker buildx use default
-docker buildx rm mybuilder
+if docker buildx inspect mybuilder; then
+    docker buildx use default
+    docker buildx rm mybuilder
+fi
--- 020_advanced/140_multi_arch/prepare.sh
+++ 020_advanced/140_multi_arch/prepare.sh
@@ -2,3 +2,10 @@
 
 # Add local registry
 docker run -d --volume $(pwd)/registry:/var/lib/registry -p 127.0.0.1:5000:5000 registry:2
+
+# Remove existing buildx builder
+export DOCKER_CLI_EXPERIMENTAL=enabled
+if docker buildx inspect mybuilder; then
+    docker buildx use default
+    docker buildx rm mybuilder
+fi
