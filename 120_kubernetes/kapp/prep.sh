a91afcdc4048914f06e22f6548a1e36baf2fa392
Nicholas Dille <nicholas.dille@mailbox.org>
2020-02-13 09:50:25 +0100

    Fixed install commands

--- 120_kubernetes/kapp/prep.sh
+++ 120_kubernetes/kapp/prep.sh
@@ -2,5 +2,5 @@
 
 curl -s https://api.github.com/repos/k14s/kapp/releases/latest | \
     jq --raw-output '.assets[] | select(.name | endswith("-linux-amd64")) | .browser_download_url' | \
-    xargs curl -sLfo ~/.local/bin/kapp
-chmod +x ~/.local/bin/kapp
+    xargs curl -sLfo /usr/local/bin/kapp
+chmod +x /usr/local/bin/kapp
--- 120_kubernetes/ytt/prep.sh
+++ 120_kubernetes/ytt/prep.sh
@@ -3,4 +3,4 @@
 curl -s https://api.github.com/repos/k14s/ytt/releases/latest | \
     jq --raw-output '.assets[] | select(.name | endswith("-linux-amd64")) | .browser_download_url' | \
     xargs curl -sLfo /usr/local/bin/ytt
-chmod +x ~/.local/bin/ytt
+chmod +x /usr/local/bin/ytt
