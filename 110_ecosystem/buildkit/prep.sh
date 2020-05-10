b46df5080d38f22d7dfb16d5364f1146cc8aca5d
Nicholas Dille <nicholas.dille@mailbox.org>
2020-05-10 22:42:24 +0200

    Fixed installation

--- 110_ecosystem/buildkit/prep.sh
+++ 110_ecosystem/buildkit/prep.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-curl -s https://api.github.com/repos/moby/buildkit/releases/latest | \
+#curl -s https://api.github.com/repos/moby/buildkit/releases/latest | \
+curl -s https://api.github.com/repos/moby/buildkit/releases | jq 'map(select(.tag_name | startswith("v"))) | .[0]' | \
     jq --raw-output '.assets[] | select(.name | endswith(".linux-amd64.tar.gz")) | .browser_download_url' | \
     xargs curl -sLf | \
     tar -xvzC /usr/local/
