bb3fa40b6c8b9b6c1269238010c21de96bec241b
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-24 22:06:42 +0200

    Added working demo of gotk

---
+++ 140_gitops/gotk/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Create cluster
+if ! kind get clusters | grep --quiet gotk; then
+    kind create cluster --name gotk --config kind.yaml
+fi
+
+# If the cluster is running on a remote Docker host
+if test "$(docker context ls | grep '*' | cut -d' ' -f1)" != "default"; then
+    SERVER=$(
+        kubectl config view --output json | \
+            jq --raw-output '
+                .clusters[] |
+                select(.name == "kind-gotk") |
+                .cluster.server
+            '
+    )
+    IP_PORT="${SERVER#https://}"
+
+    if ! ps -fC ssh | grep --quiet "${IP_PORT}:${IP_PORT}"; then
+        ssh -fNL "${IP_PORT}:${IP_PORT}" docker-hcloud
+    fi
+fi
+
+# Wait for cluster node to become ready
+echo -n "Waiting for nodes to be ready..."
+while kubectl get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.status.conditions[?(@.reason=="KubeletReady")].status}{"\n"}{end}' | grep -qE "\sFalse$"; do
+    echo -n "."
+    sleep 5
+done
+echo " done."
+
+# Install CLI
+curl -s https://pkg.dille.io/gotk/install.sh | bash
+source /usr/local/etc/bash_completion.d/gotk.sh
+
+# Deploy gotk
+gotk bootstrap github --owner=nicholasdille --repository=container-slides --branch master --path 140_gitops/gotk/state --private=false --personal=true
