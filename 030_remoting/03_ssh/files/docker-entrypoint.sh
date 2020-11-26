3da2628c3fe5a023de385e6f63368f28af74c822
Nicholas Dille <nicholas.dille@mailbox.org>
2020-11-26 23:22:23 +0100

    Improved demos

--- 030_remoting/03_ssh/files/docker-entrypoint.sh
+++ 030_remoting/03_ssh/files/docker-entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Creating the user: ${USER}"
-addgroup docker
-adduser -D ${USER} -G docker -s /bin/bash > /dev/null 2>&1
-echo "${USER}:${PASSWORD}" | chpasswd > /dev/null 2>&1
-
 echo "Generating ssh host keys..."
 ssh-keygen -A > /dev/null 2>&1
 
