5af3b6cd013cdfa0df484ea3171fa191d535556c
Nicholas Dille <nicholas.dille@mailbox.org>
2022-09-22 15:22:47 +0200

    Even more improvements

--- 160_gitlab_ci/000_rollout/bootstrap.sh
+++ 160_gitlab_ci/000_rollout/bootstrap.sh
@@ -23,7 +23,7 @@ docker compose up -d traefik gitlab
 
 GITLAB_MAX_WAIT=300
 SECONDS=0
-while ! docker compose exec --no-tty gitlab \
+while ! docker compose exec gitlab \
         curl http://localhost/-/readiness?all=1 \
             --silent \
             --fail \
@@ -40,21 +40,21 @@ echo "GitLab ready after ${SECONDS} second(s)"
 
 echo "Creating PAT for root"
 ROOT_TOKEN="$(openssl rand -hex 32)"
-if ! docker compose exec --no-tty gitlab curl http://localhost/api/v4/user \
+if ! docker compose exec gitlab curl http://localhost/api/v4/user \
         --silent \
         --fail \
         --output /dev/null \
         --header "Private-Token: ${ROOT_TOKEN}"; then
-    docker compose exec --no-tty gitlab gitlab-rails runner -e production "user = User.find_by_username('root'); token = user.personal_access_tokens.create(scopes: [:api, :read_api, :read_user, :read_repository, :write_repository, :sudo], name: 'almighty'); token.set_token('${ROOT_TOKEN}'); token.save!"
+    docker compose exec gitlab gitlab-rails runner -e production "user = User.find_by_username('root'); token = user.personal_access_tokens.create(scopes: [:api, :read_api, :read_user, :read_repository, :write_repository, :sudo], name: 'almighty'); token.set_token('${ROOT_TOKEN}'); token.save!"
 fi
 
 echo "Creating user seat"
-if ! docker compose exec --no-tty gitlab curl http://localhost/api/v4/users \
+if ! docker compose exec gitlab curl http://localhost/api/v4/users \
         --silent \
         --fail \
         --header "Private-Token: ${ROOT_TOKEN}" \
      | jq --exit-status '.[] | select(.username == "seat")' >/dev/null; then
-    docker compose exec --no-tty gitlab curl http://localhost/api/v4/users \
+    docker compose exec gitlab curl http://localhost/api/v4/users \
         --silent \
         --fail \
         --header "Private-Token: ${ROOT_TOKEN}" \
@@ -65,6 +65,6 @@ EOF
 fi
 
 echo "Retrieve runner registration token"
-export REGISTRATION_TOKEN="$(docker compose exec --no-tty gitlab gitlab-rails runner -e production "puts Gitlab::CurrentSettings.current_application_settings.runners_registration_token")"
+export REGISTRATION_TOKEN="$(docker compose exec gitlab gitlab-rails runner -e production "puts Gitlab::CurrentSettings.current_application_settings.runners_registration_token")"
 docker compose build --pull runner
 docker compose up -d runner
