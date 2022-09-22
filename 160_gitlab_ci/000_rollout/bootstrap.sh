ca18018d9754fde386144bea03e80bfd2bb07179
Nicholas Dille <nicholas.dille@mailbox.org>
2022-09-22 15:52:49 +0200

    Using proper argument

--- 160_gitlab_ci/000_rollout/bootstrap.sh
+++ 160_gitlab_ci/000_rollout/bootstrap.sh
@@ -17,6 +17,7 @@ fi
 
 echo
 echo "### Removing previous deployment"
+docker compose version
 docker compose down --volumes
 
 echo
@@ -26,28 +27,46 @@ docker compose up -d traefik gitlab
 
 echo
 echo "### Waiting for GitLab to be available"
-docker compose exec -T gitlab \
-    timeout 300 \
-        curl http://localhost/-/readiness?all=1 \
-            --silent \
-            --fail \
-            --output /dev/null
+GITLAB_MAX_WAIT=300
+SECONDS=0
+while ! docker compose exec -T gitlab curl -sfo /dev/null http://localhost/-/readiness?all=1; do
+    if test "${SECONDS}" -gt "${GITLAB_MAX_WAIT}"; then
+        echo "ERROR: Timeout waiting for GitLab to become ready."
+        exit 1
+    fi
+
+    echo "Waiting for GitLab to become ready..."
+    sleep 10
+done
+echo "GitLab ready after ${SECONDS} second(s)"
 
 echo
 echo "### Creating PAT for root"
 ROOT_TOKEN="$(openssl rand -hex 32)"
-docker compose exec -T gitlab gitlab-rails runner -e production "user = User.find_by_username('root'); token = user.personal_access_tokens.create(scopes: [:api, :read_api, :read_user, :read_repository, :write_repository, :sudo], name: 'almighty'); token.set_token('${ROOT_TOKEN}'); token.save!"
+if ! docker compose exec -T gitlab curl http://localhost/api/v4/user \
+        --silent \
+        --fail \
+        --output /dev/null \
+        --header "Private-Token: ${ROOT_TOKEN}"; then
+    docker compose exec -T gitlab gitlab-rails runner -e production "user = User.find_by_username('root'); token = user.personal_access_tokens.create(scopes: [:api, :read_api, :read_user, :read_repository, :write_repository, :sudo], name: 'almighty'); token.set_token('${ROOT_TOKEN}'); token.save!"
+fi
 
 echo
 echo "### Creating user seat"
-docker compose exec -T gitlab \
-    curl http://localhost/api/v4/users \
+if ! docker compose exec -T gitlab curl http://localhost/api/v4/users \
+        --silent \
+        --fail \
+        --header "Private-Token: ${ROOT_TOKEN}" \
+     | jq --exit-status '.[] | select(.username == "seat")' >/dev/null; then
+    docker compose exec -T gitlab curl http://localhost/api/v4/users \
         --silent \
         --fail \
         --header "Private-Token: ${ROOT_TOKEN}" \
         --header "Content-Type: application/json" \
         --request POST \
         --data "{\"username\": \"seat\", \"name\": \"seat\", \"email\": \"seat@${DOMAIN}\", \"password\": \"${SEAT_PASS}\", \"skip_confirmation\": \"true\", \"admin\": \"true\"}"
+EOF
+fi
 
 echo
 echo "### Retrieving runner registration token"
