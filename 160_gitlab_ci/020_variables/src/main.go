706c7fec438f4cf6194d318aed063ad3303f9365
Nicholas Dille <nicholas.dille@mailbox.org>
2022-10-13 10:49:31 +0200

    Even more fixes

--- 160_gitlab_ci/020_variables/src/main.go
+++ 160_gitlab_ci/020_variables/src/main.go
@@ -1,12 +1,14 @@
 package main
 
-import fmt
-import "github.com/TwiN/go-color"
+import (
+	"fmt"
+	"github.com/TwiN/go-color"
+)
 
 var Author string = "unknown"
 var Version string = "none"
 
 func main() {
 	println(color.InGreen("hello world"))
-	fmt.Sprintf("by %s, version %s", Author, Version)
+	fmt.Printf("by %s, version %s", Author, Version)
 }
