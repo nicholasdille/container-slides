e49428e821374bdc93ea4e65fe1c1987d1edc219
Nicholas Dille <nicholas.dille@mailbox.org>
2023-05-02 17:24:11 +0200

    Updated commands and fixes

--- 060_security/11_artifacts/run.sh
+++ 060_security/11_artifacts/run.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 set -o errexit
 
+REGISTRY=127.0.0.1:5000
+REPOSITORY=ubuntu
+TAG=22.04
+
 if test "$(docker container ls --all --filter status=exited --filter name=registry | wc -l)" -gt 1; then
     docker container rm registry
 fi
 if test "$(docker container ls --filter name=registry | wc -l)" -eq 1; then
     # TODO: Use OCI 1.1 compliant registry (https://github.com/oras-project/distribution/pkgs/container/registry)
-    docker container run --detach --name registry --publish 127.0.0.1:5000:5000 registry
+    docker container run --detach --name registry --publish "${REGISTRY}:5000" ghcr.io/oras-project/registry:v1.0.0-rc.4
 fi
-regctl registry set 127.0.0.1:5000 --tls disabled
+regctl registry set "${REGISTRY}" --tls disabled
 
-IMAGE=127.0.0.1:5000/ubuntu:22.04
-docker pull ubuntu:22.04
-docker tag ubuntu:22.04 "${IMAGE}"
+IMAGE="${REGISTRY}/${REPOSITORY}:${TAG}"
+docker pull "${REPOSITORY}:${TAG}"
+docker tag "${REPOSITORY}:${TAG}" "${IMAGE}"
 docker push "${IMAGE}"
 
 trivy image "${IMAGE}" --format cyclonedx --output cyclonedx.json
 cat cyclonedx.json \
 | regctl artifact put --subject "${IMAGE}" \
     --artifact-type application/vnd.cyclonedx+json \
-    --file-media-type cyclonedx.json \
+    --file-media-type application/vnd.cyclonedx+json \
     --annotation "created-by=trivy" \
-    --annotation "created=$(date -Iseconds)"
+    --annotation "org.opencontainers.artifact.created=$(date -Iseconds)" \
+    --annotation "org.opencontainers.artifact.description=CycloneDX JSON SBOM"
 #cat cyclonedx.json \
 #| trivy referrer put
 #oras push "${IMAGE}" \
 #    --artifact-type 'signature/example' \
 #    --subject "${IMAGE}" \
 #    ./cyclonedx.json:application/vnd.cyclonedx+json
+#cosign attach sbom ...
 trivy image "${IMAGE}" --format sarif \
 | trivy referrer put
 
@@ -36,6 +42,12 @@ trivy referrer list "${IMAGE}" --format table
 regctl artifact list "${IMAGE}"
 regctl artifact tree "${IMAGE}"
 oras discover "${IMAGE}" --plain-http --output tree
+
+IMAGE_DIGEST="$(
+    curl -svH "Accept: application/vnd.docker.distribution.manifest.v2+json" "http://${REGISTRY}/v2/${REPOSITORY}/manifests/${TAG}" 2>&1 \
+    | grep "^< Docker-Content-Digest:" | cut -d: -f2- | tr -d ' ' | tr -d '\r'
+)"
+curl -s "http://${REGISTRY}/v2/${REPOSITORY}/referrers/${IMAGE_DIGEST}" | jq .
 
 trivy image "${IMAGE}" --sbom-sources oci
 
@@ -49,6 +61,7 @@ fi
 COSIGN_KEY=./cosign.key
 COSIGN_PUB=./cosign.pub
 COSIGN_EXPERIMENTAL=1 cosign sign -y --key "${COSIGN_KEY}" --registry-referrers-mode oci-1-1 "${IMAGE}"
+#cosign attach signature ...
 
 SOURCE_CYCLONEDX="$(
     regctl artifact tree --filter-artifact-type application/vnd.cyclonedx+json "${IMAGE}" --format "{{json .}}" \
