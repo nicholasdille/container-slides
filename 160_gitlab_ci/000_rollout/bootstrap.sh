c47be91a4fa6148a8519c5ac4c15c7de20e6cba8
Nicholas Dille <nicholas.dille@mailbox.org>
2023-11-10 08:29:06 +0100

    Fixes

--- 160_gitlab_ci/000_rollout/bootstrap.sh
+++ 160_gitlab_ci/000_rollout/bootstrap.sh
@@ -26,7 +26,7 @@ export REGISTRATION_TOKEN=foo
 GITLAB_MAX_WAIT=300
 SECONDS=0
 while ! docker compose exec -T gitlab \
-        curl -sfo /dev/null http://localhost/-/readiness?all=1; do
+        curl -sSfo /dev/null http://localhost/-/readiness?all=1; do
     if test "${SECONDS}" -gt "${GITLAB_MAX_WAIT}"; then
         echo "ERROR: Timeout waiting for GitLab to become ready."
         exit 1
@@ -45,6 +45,7 @@ if ! docker compose exec -T gitlab \
         curl \
             --url http://localhost/api/v4/user \
             --silent \
+            --show-error \
             --fail \
             --output /dev/null \
             --header "Private-Token: ${ROOT_TOKEN}"; then
@@ -58,6 +59,7 @@ docker compose exec -T gitlab \
     curl \
         --url http://localhost/api/v4/application/settings?signup_enabled=false \
         --silent \
+        --show-error \
         --fail \
         --header "Private-Token: ${ROOT_TOKEN}" \
         --header "Content-Type: application/json" \
@@ -70,12 +72,14 @@ if ! docker compose exec -T gitlab \
         curl \
             --url http://localhost/api/v4/users \
             --silent \
+            --show-error \
             --fail \
             --header "Private-Token: ${ROOT_TOKEN}" \
      | jq --exit-status '.[] | select(.username == "seat")' >/dev/null; then
     docker compose exec -T gitlab \
         curl http://localhost/api/v4/users \
             --silent \
+            --show-error \
             --fail \
             --header "Private-Token: ${ROOT_TOKEN}" \
             --header "Content-Type: application/json" \
@@ -91,25 +95,28 @@ if ! docker compose exec -T gitlab \
         curl \
             --url http://localhost/api/v4/user \
             --silent \
+            --show-error \
             --fail \
             --output /dev/null \
             --header "Private-Token: ${SEAT_TOKEN}"; then
     docker compose exec -T gitlab \
-        gitlab-rails runner -e production "user = User.find_by_username('seat'); token = user.personal_access_tokens.create(scopes: [:api, :read_api, :read_user, :read_repository, :write_repository], name: 'demo'token = user.personal_access_tokens.create(scopes: [:api, :read_api, :read_user, :read_repository, :write_repository, :sudo], name: 'almighty', expires_at: 365.days.from_now); token.set_token('${SEAT_TOKEN}'); token.save!"
+        gitlab-rails runner -e production "user = User.find_by_username('seat'); token = user.personal_access_tokens.create(scopes: [:api, :read_api, :read_user, :read_repository, :write_repository], name: 'demo', expires_at: 365.days.from_now); token.set_token('${SEAT_TOKEN}'); token.save!"
 fi
 
 echo
 echo "### Creating project for demos"
 if ! docker compose exec -T gitlab \
     curl \
-         --url http://localhost/api/v4/users/seat/projects \
-         --silent \
-         --header "Private-Token: ${SEAT_TOKEN}" \
+        --url http://localhost/api/v4/users/seat/projects \
+        --silent \
+        --show-error \
+        --header "Private-Token: ${SEAT_TOKEN}" \
     | jq --exit-status '.[] | select(.name == "demo")' >/dev/null; then
     docker compose exec -T gitlab \
         curl \
             --url "http://localhost/api/v4/projects" \
             --silent \
+            --show-error \
             --request POST \
             --header "Private-Token: ${SEAT_TOKEN}" \
             --header "Content-Type: application/json" \
@@ -119,6 +126,7 @@ if ! docker compose exec -T gitlab \
                 curl \
                 --url "http://localhost/api/v4/projects/seat%2fdemo" \
                 --silent \
+                --show-error \
                 --header "Private-Token: ${SEAT_TOKEN}" \
             | jq --exit-status 'select(.import_status == "finished")' >/dev/null 2>&1; do
         if test "${SECONDS}" -gt 60; then
@@ -130,6 +138,7 @@ if ! docker compose exec -T gitlab \
             curl \
                 --url "http://localhost/api/v4/projects/seat%2fdemo" \
                 --silent \
+                --show-error \
                 --header "Private-Token: ${SEAT_TOKEN}" \
             | jq --raw-output '.import_status'
         sleep 5
@@ -158,6 +167,7 @@ docker compose exec -T gitlab \
     curl \
         --url "http://localhost/api/v4/projects/seat%2fdemo" \
         --silent \
+        --show-error \
         --request PUT \
         --header "Private-Token: ${SEAT_TOKEN}" \
         --data 'default_branch=main'
