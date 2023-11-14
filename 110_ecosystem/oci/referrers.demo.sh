82ad83c9742ad18834ed057619e5d7b804616eae
Nicholas Dille <nicholas.dille@mailbox.org>
2023-11-15 00:16:52 +0100

    Improved demo

--- 110_ecosystem/oci/referrers.demo.sh
+++ 110_ecosystem/oci/referrers.demo.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 set -o errexit
 
+echo
+echo "--------------------------------------------------"
 echo "Starting container registry..."
 docker container run --detach --name registry --publish 127.0.0.1:5000:5000 ghcr.io/oras-project/registry:v1.0.0-rc.4
 
+echo
+echo "--------------------------------------------------"
 echo "Configure regctl for registry..."
 regctl registry set 127.0.0.1:5000 --tls=disabled
 
+echo
+echo "--------------------------------------------------"
 echo "Build and push image..."
-docker build --tag localhost:5000/ubuntu:22.04 --provenance=false --output=type=registry,oci-mediatypes=true .
+docker build --quiet --tag localhost:5000/ubuntu:22.04 --provenance=false --output=type=registry,oci-mediatypes=true .
 DIGEST="$(regctl manifest head 127.0.0.1:5000/ubuntu:22.04)"
 
+echo
+echo "--------------------------------------------------"
 echo "Check if referrers are supported..."
-if curl -sf "http://127.0.0.1:5000/v2/ubuntu/referrers/${DIGEST}"; then
+if curl -sfo /dev/null "http://127.0.0.1:5000/v2/ubuntu/referrers/${DIGEST}"; then
     echo "Referrers are supported!"
 fi
 
+echo
+echo "--------------------------------------------------"
 echo "Create SBOM for image..."
-trivy image 127.0.0.1:5000/ubuntu:22.04 --format cyclonedx --output cyclonedx.json
+trivy --quiet image 127.0.0.1:5000/ubuntu:22.04 --format cyclonedx --output cyclonedx.json
 
+echo
+echo "--------------------------------------------------"
 echo "Push SBOM as artifact..."
 regctl artifact put \
     --file cyclonedx.json \
@@ -28,12 +40,19 @@ regctl artifact put \
     --annotation "org.opencontainers.artifact.created=$(date -Iseconds)" \
     --annotation "org.opencontainers.artifact.description=CycloneDX JSON SBOM"
 
+echo
+echo "--------------------------------------------------"
 echo "Current state of artifact tree:"
 regctl artifact list 127.0.0.1:5000/ubuntu:22.04
+read
 
+echo
+echo "--------------------------------------------------"
 echo "Create SARIF report..."
-trivy image 127.0.0.1:5000/ubuntu:22.04 --format sarif --output sarif.json
+trivy --quiet image 127.0.0.1:5000/ubuntu:22.04 --format sarif --output sarif.json
 
+echo
+echo "--------------------------------------------------"
 echo "Push SARIF report as artifact..."
 regctl artifact put \
     --file sarif.json \
@@ -43,16 +62,26 @@ regctl artifact put \
     --annotation "org.opencontainers.artifact.created=$(date -Iseconds)" \
     --annotation "org.opencontainers.artifact.description=SARIF JSON"
 
+echo
+echo "--------------------------------------------------"
 echo "Current state of artifact tree:"
 regctl artifact list 127.0.0.1:5000/ubuntu:22.04
+read
 
+echo
+echo "--------------------------------------------------"
 echo "Create temporary cosign key pair..."
+rm cosign.key cosign.pub
 export COSIGN_PASSWORD="$(openssl rand -hex 32)"
 cosign generate-key-pair
 
+echo
+echo "--------------------------------------------------"
 echo "Sign image..."
 COSIGN_EXPERIMENTAL=1 cosign sign --yes --key=cosign.key --registry-referrers-mode=oci-1-1 127.0.0.1:5000/ubuntu:22.04
 
+echo
+echo "--------------------------------------------------"
 echo "Sign SBOM..."
 DIGEST_SBOM="$(
     regctl artifact tree 127.0.0.1:5000/ubuntu:22.04 --format "{{json .}}" \
@@ -65,9 +94,14 @@ DIGEST_SBOM="$(
 )"
 COSIGN_EXPERIMENTAL=1 cosign sign --yes --key=cosign.key --registry-referrers-mode=oci-1-1 "127.0.0.1:5000/ubuntu:22.04@${DIGEST_SBOM}"
 
+echo
+echo "--------------------------------------------------"
 echo "Current state of artifact tree:"
 regctl artifact tree 127.0.0.1:5000/ubuntu:22.04
+read
 
+echo
+echo "--------------------------------------------------"
 echo "Sign SARIF report..."
 DIGEST_SARIF="$(
     regctl artifact tree 127.0.0.1:5000/ubuntu:22.04 --format "{{json .}}" \
@@ -80,11 +114,18 @@ DIGEST_SARIF="$(
 )"
 COSIGN_EXPERIMENTAL=1 cosign sign --yes --key=cosign.key --registry-referrers-mode=oci-1-1 "127.0.0.1:5000/ubuntu:22.04@${DIGEST_SARIF}"
 
+echo
+echo "--------------------------------------------------"
 echo "Current state of artifact tree:"
 regctl artifact tree 127.0.0.1:5000/ubuntu:22.04
+read
 
+echo
+echo "--------------------------------------------------"
 echo "Get SBOM artifact..."
 regctl artifact get "127.0.0.1:5000/ubuntu@${DIGEST_SBOM}" | jless
 
+echo
+echo "--------------------------------------------------"
 echo "Inspect manifest for SBOM artifact..."
 regctl manifest get "127.0.0.1:5000/ubuntu@${DIGEST_SBOM}" --format "{{json .}}" | jless
