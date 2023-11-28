c9a9c8fdb3cac8ff04c219ead4da33395705c295
Nicholas Dille <nicholas.dille@mailbox.org>
2023-11-28 16:52:01 +0100

    Fixed random order of entrypoint.d scripts

--- 160_gitlab_ci/000_rollout/vscode/vscode/git.sh
+++ 160_gitlab_ci/000_rollout/vscode/vscode/git.sh
@@ -9,3 +9,6 @@ git config --global user.name "${GIT_USER}"
 git config --global user.email "${GIT_EMAIL}"
 git config --global credential.helper store
 echo "${GIT_CRED}" >.git-credentials
+
+git clone "https://gitlab.inmylab.de/${GIT_USER}/demo" /home/seat/demo
+git -C /home/seat/demo remote add upstream https://github.com/nicholasdille/container-slides
--- 160_gitlab_ci/000_rollout/vscode/vscode/repository.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git clone "https://gitlab.inmylab.de/${GIT_USER}/demo" /home/seat/demo
-git -C /home/seat/demo remote add upstream https://github.com/nicholasdille/container-slides
