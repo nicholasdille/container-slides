e837c3005506411c004805d4435056f3723983fc
Nicholas Dille <nicholas.dille@mailbox.org>
2023-12-09 23:24:15 +0100

    Test helmfile

--- 170_supply_chain_security/sbom-operator/prepare.sh
+++ 170_supply_chain_security/sbom-operator/prepare.sh
@@ -2,25 +2,22 @@
 set -o errexit
 
 # tools
-uniget install docker buildx docker-compose kind helm kubectl cosign
+uniget install docker buildx docker-compose kind helm kubectl cosign helmfile
 
 # cluster
 kind create cluster --config kind.yaml
-
-# ingress
-helm repo add traefik https://traefik.github.io/charts
-helm upgrade --install traefik traefik/traefik --values values-traefik.yaml
-kubectl apply -f ingress.yaml
 
 # prometheus-operator
 curl -sL https://github.com/prometheus-operator/prometheus-operator/releases/latest/download/bundle.yaml | kubectl create -f -
 kubectl apply -f prometheus.yaml
 
-# grafana
-helm repo add grafana https://grafana.github.io/helm-charts
-helm --namespace kube-system upgrade --install grafana grafana/grafana --values values-grafana.yaml
-kubectl -n kube-system get secret grafana -o json | jq -r '.data."admin-password"' | base64 -d
-
 # sbom-system
 kubectl create namespace sbom-system
 kubectl -n sbom-system create secret generic sbom-operator --from-literal=accessToken=TOKEN
+
+# helmfile
+helmfile apply
+kubectl apply -f ingress.yaml
+
+# grafana admin password
+kubectl -n kube-system get secret grafana -o json | jq -r '.data."admin-password"' | base64 -d
