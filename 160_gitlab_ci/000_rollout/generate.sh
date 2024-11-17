e824a258193a51b9bf8547e6755c23e829d6824b
Nicholas Dille <nicholas.dille@mailbox.org>
2024-11-17 22:23:32 +0100

    Prep for 20241121

--- 160_gitlab_ci/000_rollout/generate.sh
+++ 160_gitlab_ci/000_rollout/generate.sh
@@ -5,11 +5,6 @@ set -o errexit -o pipefail
 : "${COUNT:=1}"
 : "${DOMAIN:=inmylab.de}"
 
-if test -f seats.json; then
-    echo "ERROR: File seats.json already exists."
-    exit 1
-fi
-
 function generate_code() {
     local chars=(A B C D E F G H J K L M N P Q R S T U V W X Y Z 2 3 4 5 6 7 8 9)
     local length=6
@@ -26,50 +21,32 @@ function generate_code() {
     echo -n "${code}"
 }
 
-result="$(
-    jq \
-        --null-input \
-        --arg name "${SET_NAME}" \
-        --arg count "${COUNT}" \
-        --arg domain "${DOMAIN}" \
-        --arg gitlab_admin_password "$(openssl rand -hex 32)" \
-        --arg gitlab_admin_token "$(openssl rand -hex 32)" \
-        '
-        {
-            "name": $name,
-            "count": $count,
-            "domain": $domain,
-            "gitlab_admin_password": $gitlab_admin_password,
-            "gitlab_admin_token": $gitlab_admin_token,
-            "seats": []
-        }
-        '
-)"
-
-for INDEX in $( seq 0 $(( COUNT - 1)) ); do
-    result="$(
-        echo "${result}" | jq \
-            --arg index "${INDEX}" \
-            --arg password "$(openssl rand -hex 32)" \
-            --arg code "$(generate_code)" \
-            --arg gitlab_token "$(openssl rand -hex 32)" \
-            --arg webdav_pass_dev "$(openssl rand -hex 32)" \
-            --arg webdav_pass_live "$(openssl rand -hex 32)" \
-            '
-            . as $all |
-            $all.seats +=
-            [{
-                "index": $index,
-                "password": $password,
-                "code": $code,
-                "gitlab_token": $gitlab_token,
-                "webdav_pass_dev": $webdav_pass_dev,
-                "webdav_pass_live": $webdav_pass_live
-            }]
-            '
-    )"
-done
-echo "${result}" >seats.json
+if ! test -f seats.json; then
+    for INDEX in $( seq 0 $(( COUNT - 1)) ); do
+        result="$(
+            echo "${result}" | jq \
+                --arg index "${INDEX}" \
+                --arg password "$(openssl rand -hex 32)" \
+                --arg code "$(generate_code)" \
+                --arg gitlab_token "$(openssl rand -hex 32)" \
+                --arg webdav_pass_dev "$(openssl rand -hex 32)" \
+                --arg webdav_pass_live "$(openssl rand -hex 32)" \
+                '
+                . as $all |
+                $all.seats +=
+                [{
+                    "index": $index,
+                    "password": $password,
+                    "code": $code,
+                    "gitlab_token": $gitlab_token,
+                    "webdav_pass_dev": $webdav_pass_dev,
+                    "webdav_pass_live": $webdav_pass_live
+                }]
+                '
+        )"
+    done
+    echo "${result}" >seats.json
+fi
 
 UNIQUE_CODES_COUNT="$(
     jq --raw-output '.seats[].code' seats.json \
