4d96c203e1c0704710ae63e378bd1d2093a6c5ea
Nicholas Dille <nicholas.dille@mailbox.org>
2024-11-20 19:51:46 +0100

    Added output for runner token

--- 160_gitlab_ci/001_gitlab/outputs.tf
+++ 160_gitlab_ci/001_gitlab/outputs.tf
@@ -2,3 +2,8 @@ output "personal_access_tokens" {
     value = gitlab_personal_access_token.seats_vscode[*].token
     sensitive = true
 }
+
+output "runner_token" {
+    value = gitlab_user_runner.shared.token
+    sensitive = true
+}
