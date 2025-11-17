0dc55560b272b212f1b7e51a2b85aafd6e15172d
Nicholas Dille <nicholas.dille@mailbox.org>
2025-11-17 09:17:36 +0100

    Bumped terraform providers

--- 160_gitlab_ci/001_server/versions.tf
+++ 160_gitlab_ci/001_server/versions.tf
@@ -3,12 +3,12 @@ terraform {
     gitlab = {
       # https://registry.terraform.io/providers/gitlabhq/gitlab/latest/docs
       source = "gitlabhq/gitlab"
-      version = "18.2.0"
+      version = "18.5.0"
     }
     grafana = {
       # https://registry.terraform.io/providers/grafana/grafana/latest/docs
       source = "grafana/grafana"
-      version = "3.25.9"
+      version = "4.14.0"
     }
   }
 }
