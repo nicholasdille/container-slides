ac3522710f7da7984860d6dea5165cd26fddcc06
Nicholas Dille <nicholas.dille@mailbox.org>
2025-11-25 13:14:37 +0100

    Added Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,35 +1,32 @@
-var gulp = require('gulp');
-var connect = require('connect');
-var connectLivereload = require('connect-livereload');
-var opn = require('opn');
-var gulpLivereload = require('gulp-livereload');
+var gulp 				= require('gulp')
+var browserSync = require('browser-sync').create()
+var reload			= browserSync.reload
 
-var config = {
-    rootDir: __dirname,
-    servingPort: 8080,
+var files = [
+  '*.html',
+  '**/*.md',
+  'themes/*.css',
+  '**/*.{svg,png,jpg,gif}',
 
-    // the files you want to watch for changes for live reload
-    filesToWatch: ['*.{html,css,js}', '!Gulpfile.js']
-}
+  '!node_modules/**/*',
+];
 
-// The default task - called when you run `gulp` from CLI
-gulp.task('default', ['watch', 'serve']);
-
-gulp.task('watch', ['connect'], function () {
-  gulpLivereload.listen();
-  gulp.watch(config.filesToWatch, function(file) {
-    gulp.src(file.path)
-      .pipe(gulpLivereload());
-  });
+gulp.task('browser-sync', function() {
+	browserSync.init(files, {
+		// Read here http://www.browsersync.io/docs/options/
+		server: {
+			baseDir: "./",
+      directory: true
+		},
+		notify: false,
+		port: 8080,
+    localOnly: true,
+		injectChanges: true,
+    open: "local",
+    ghostMode: false
+	})
 });
 
-gulp.task('serve', ['connect'], function () {
-  return opn('http://localhost:' + config.servingPort);
-});
-
-gulp.task('connect', function(){
-  return connect()
-    .use(connectLivereload())
-    .use(connect.static(config.rootDir))
-    .listen(config.servingPort);
+gulp.task('default', gulp.series('browser-sync'), function () {
+  gulp.watch([files], browserSync.reload);
 });
