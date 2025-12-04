875bd73619acae53eeccae8adabb640bdb279790
Nicholas Dille <nicholas.dille@mailbox.org>
2025-12-04 22:27:01 +0100

    Updated example app

--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 	"github.com/TwiN/go-color"
 )
 
-var Author string = "unknown"
 var Version string = "none"
 
 func main() {
 	println(color.InGreen("hello world"))
-	fmt.Printf("by %s, version %s", Author, Version)
+	fmt.Printf(
+		"version %s",
+		Version,
+	)
+	println()
 }
