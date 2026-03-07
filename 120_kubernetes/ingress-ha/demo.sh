31abf0077855d988995fd83bdb8c3d9a5ecedacd
Nicholas Dille <nicholas.dille@mailbox.org>
2026-03-07 10:24:44 +0100

    Updated after second poc

--- 120_kubernetes/ingress-ha/demo.sh
+++ 120_kubernetes/ingress-ha/demo.sh
@@ -8,9 +8,10 @@ kind create cluster \
 # Deploy Cilium with...
 # - kube-proxy replacement
 # - Layer 2 Announcement (LoadBalancer)
+helm repo add cilium https://helm.cilium.io/
 helm upgrade --namespace kube-system --install \
     cilium cilium/cilium \
-        --set kubeProxyReplacement=strict \
+        --set kubeProxyReplacement=true \
         --set l2announcements.enabled=true \
         --set k8sServiceHost=kind-control-plane \
         --set k8sServicePort=6443 \
@@ -20,27 +21,29 @@ helm upgrade --namespace kube-system --install \
 
 # Create IP pool
 cat <<EOF | kubectl apply -f -
-apiVersion: cilium.io/v2alpha1
+apiVersion: cilium.io/v2
 kind: CiliumLoadBalancerIPPool
 metadata:
   name: test
 spec:
   blocks:
-  - start: 10.222.100.10
-    stop: 10.222.100.30
+  - start: 100.64.1.250
+    stop: 100.64.1.255
 EOF
 
 # Deploy traefik
+helm repo add traefik https://traefik.github.io/charts
 helm upgrade --namespace kube-system --install \
     traefik traefik/traefik \
         --set deployment.replicas=2
 
 # Deploy test service
+helm repo add stefanprodan https://stefanprodan.github.io/podinfo
 helm upgrade --install \
-    podinfo podinfo/podinfo \
+    podinfo stefanprodan/podinfo \
         --set replicaCount=2 \
         --set ingress.enabled=true
 
 # Test ingress
 docker exec -it kind-control-plane \
-    curl --verbose --resolve podinfo.local:80:10.222.100.11 http://podinfo.local/
+    curl --verbose --resolve podinfo.local:80:100.64.1.250 http://podinfo.local/
